0d1a340fa0e9cd317e29a929d0ef117b5e6a3224
GamerIcePlayBence <gamerbence90@gmail.com>
2025-02-15 16:15:25 +0100

    Create load and Unload command

---
+++ src/main/java/frc/robot/commands/LoadCoralCommand.java
@@ -0,0 +1,46 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+package frc.robot.commands;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.CorallIntake.CoralDeliverySubsystem;
+
+public class LoadCoralCommand extends Command{
+    @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+    private CoralDeliverySubsystem coraldelivery;
+
+
+  /**
+   * Creates a new ExampleCommand.
+   *
+   * @param subsystem The subsystem used by this command.
+   */
+  public void LoadCoral() {
+    coraldelivery.setDeliveryPower(1);
+  }
+
+  public void UnloadCoral() {
+    coraldelivery.setDeliveryPower(-1);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    LoadCoral();
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+
+}
