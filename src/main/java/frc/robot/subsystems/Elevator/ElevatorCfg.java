11e76b089ef25442b862a9b289d2a201d5fde241
adametoth <adamtoth2007@gmail.com>
2025-03-01 16:16:59 +0100

    Changed gear ratio

--- src/main/java/frc/robot/subsystems/Elevator/ElevatorCfg.java
+++ src/main/java/frc/robot/subsystems/Elevator/ElevatorCfg.java
@@ -9,7 +9,7 @@ public class ElevatorCfg {
 
     public static final SparkFlex ELEVATOR_MOTOR = new SparkFlex(ELEVATOR_MOTOR_ID, SparkLowLevel.MotorType.kBrushless);
 
-    public static final double ELEVATOR_GEAR_RATIO = 1.0/9.0;
+    public static final double ELEVATOR_GEAR_RATIO = 1.0/27.0;
     public static final double SPROCKET_DIA_CM = 4.47;
     public static final double ELEVATOR_POS_CONVERSION_CM = 2*(ELEVATOR_GEAR_RATIO*SPROCKET_DIA_CM*Math.PI);
 
@@ -27,7 +27,7 @@ public class ElevatorCfg {
     public static final double ELEVATOR_LONE_POSITION = 50;
     public static final double ELEVATOR_LTWO_POSITION = 70;
     public static final double ELEVATOR_LTHREE_POSITION = 90;
-    public static final double ELEVATOR_LFOUR_POSITION =110;
+    public static final double ELEVATOR_LFOUR_POSITION =115;
 
     public static final double ELEVATOR_POSITIONS[] = {ELEVATOR_LOAD_POSITION, ELEVATOR_LONE_POSITION, ELEVATOR_LTWO_POSITION, ELEVATOR_LTHREE_POSITION, ELEVATOR_LFOUR_POSITION};
 }
