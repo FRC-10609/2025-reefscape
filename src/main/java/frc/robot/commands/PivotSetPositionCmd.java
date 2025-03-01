0ab60f01721f0dd196d14eba57d31833adca5449
olivermtoth <oliverm.toth@protonmail.ch>
2025-03-01 10:25:52 +0100

    Fix errors after merge; naming, lost code

--- src/main/java/frc/robot/commands/PivotSetPositionCmd.java
+++ src/main/java/frc/robot/commands/PivotSetPositionCmd.java
@@ -1,7 +1,7 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.subsystems.CorallIntake.CoralDeliverySubsystem;
+import frc.robot.subsystems.CoralDelivery.CoralDeliverySubsystem;
 
 public class PivotSetPositionCmd extends Command{
     private final CoralDeliverySubsystem pivotSubsystem;
