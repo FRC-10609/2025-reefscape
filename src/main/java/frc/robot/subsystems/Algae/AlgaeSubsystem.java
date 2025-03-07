0efb5c2272351738e7bfdaca6f8f6f1048458945
adametoth <adamtoth2007@gmail.com>
2025-03-07 20:54:44 +0100

    changed algae subsystem + command to suit the new role

--- src/main/java/frc/robot/subsystems/Algae/AlgaeSubsystem.java
+++ src/main/java/frc/robot/subsystems/Algae/AlgaeSubsystem.java
@@ -2,39 +2,83 @@ package frc.robot.subsystems.Algae;
 
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkBase;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkFlex;
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.ClosedLoopConfig;
 import com.revrobotics.spark.config.EncoderConfig;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import frc.robot.subsystems.Algae.AlgaeCfg;
 
 public class AlgaeSubsystem extends SubsystemBase  {
-    private final SparkMax leftMotor;
-    private final SparkMax rightMotor;
+    private final SparkMax algaeMotor;
+    private final SparkClosedLoopController algaePIDController;
+    private final double algae_p_gain = AlgaeCfg.ALGAE_P_GAIN;
+    private final double algae_i_gain = AlgaeCfg.ALGAE_I_GAIN;
+    private final double algae_d_gain = AlgaeCfg.ALGAE_D_GAIN;
+    private final RelativeEncoder algaeEncoder;
+    private int algaePos;
 
 
     public AlgaeSubsystem() {
-        leftMotor = AlgaeCfg.LEFT_MOTOR;
-        rightMotor = AlgaeCfg.RIGHT_MOTOR;
-        
-        SparkMaxConfig leftConfig = new SparkMaxConfig();
-        leftConfig.idleMode(AlgaeCfg.LEFT_IDLE_MODE);
-        leftConfig.inverted(AlgaeCfg.LEFT_INVERTED);
-        leftConfig.smartCurrentLimit(AlgaeCfg.LEFT_AMP_LIMIT);
-        leftMotor.configure(leftConfig, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters);
-        
-        SparkMaxConfig rightConfig = new SparkMaxConfig();
-        rightConfig.idleMode(AlgaeCfg.RIGHT_IDLE_MODE);
-        rightConfig.inverted(AlgaeCfg.RIGHT_INVERTED);
-        rightConfig.smartCurrentLimit(AlgaeCfg.RIGHT_AMP_LIMIT);
-        rightMotor.configure(rightConfig, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters);
-        
+        algaeMotor = AlgaeCfg.MOTOR;
+        algaeEncoder = algaeMotor.getEncoder();
+        algaePIDController = algaeMotor.getClosedLoopController();
+        algaePos = 0;
+
+        config();            
     }
 
-    public void inTake(int in){
-        leftMotor.set(in);
-        rightMotor.set(in);
+    private void config(){
+        SparkMaxConfig algaeConfig = new SparkMaxConfig();
+        algaeConfig.idleMode(AlgaeCfg.ALGAE_IDLE_MODE);
+        algaeConfig.inverted(AlgaeCfg.ALGAE_INVERTED);
+        algaeConfig.smartCurrentLimit(AlgaeCfg.ALGAE_AMP_LIMIT);
+        
+        ClosedLoopConfig algaePIDConfig = new ClosedLoopConfig();
+        algaePIDConfig.p(algae_p_gain);
+        algaePIDConfig.i(algae_i_gain);
+        algaePIDConfig.d(algae_d_gain);
+        algaePIDConfig.outputRange(-0.3, 0.6);
+        algaeConfig.apply(algaePIDConfig);
+
+        EncoderConfig algaeEncoderConfig = new EncoderConfig();
+        algaeEncoderConfig.velocityConversionFactor(AlgaeCfg.GEAR_RATIO);
+        algaeEncoderConfig.positionConversionFactor(AlgaeCfg.GEAR_RATIO);
+        algaeConfig.apply(algaeEncoderConfig);
+        
+        algaeMotor.configure(algaeConfig, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters);
+
     }
+    
+    @Override
+    public void periodic(){
+        updateDashboard();
+        setAlgaePosition(algaePos);
+    }
+    
+    public double getPosition(){
+        return algaeEncoder.getPosition();
+    }
+    
+    private void updateDashboard(){
+        SmartDashboard.putNumber("Algae SetPosition: ", algaePos);
+        SmartDashboard.putNumber("Algae Position: ", getPosition());
+    }
+
+    private void setPosition(double pos){
+        algaePIDController.setReference(pos, SparkFlex.ControlType.kPosition);
+    }
+
+    public void setAlgaePosition(int pos){
+        setPosition(AlgaeCfg.ALGAE_POSITIONS[pos]);
+    }
+    
+
+    
+    
 }
